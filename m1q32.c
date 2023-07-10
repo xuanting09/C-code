d6c1d184f0af309ef09e52aed7f6cf0e136974e1
xuanting09 <0912shenny@gemail.com>
2023-07-11 00:42:07 +0800

    0711Exercises

---
+++ m1q32.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int price;
+    printf("請輸入價格（1-1000）：");
+    scanf("%d", &price);
+
+    int thousand = price / 1000;
+    int hundred = (price % 1000) / 100;
+    int ten = (price % 100) / 10;
+    int dollar = price % 10;
+
+    if (thousand > 0) {
+        printf("%d thousand ", thousand);
+    }
+    if (hundred > 0) {
+        printf("%d hundred ", hundred);
+    }
+    if (ten > 0) {
+        printf("%d ten ", ten);
+    }
+    if (dollar > 0) {
+        printf("%d dollar ", dollar);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
